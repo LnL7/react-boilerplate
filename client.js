ceacd4516d71e3279baeebe2487ecd8702d97ec9
Daiderd Jordan <daiderd@gmail.com>
2015-03-10 13:08:05 +0100

    use node_modules paths

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
-var entry  = require('./app/client/entry')
-  , routes = require('./app/router/routes')
+var entry  = require('app/client/entry')
+  , routes = require('app/router/routes')
   ;
 
 entry(routes);
